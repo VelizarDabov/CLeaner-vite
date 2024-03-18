08b6b4f40f3f443fab093bb693e845f8b62918b6
Velizar Dabov <velizardabov@gmail.com>
2024-03-18 13:36:39 +0200

    service section ready

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -23,7 +23,7 @@ const Home = () => {
     
     <>
  
-        <section
+        <section id='home'
     className="pt-[7rem] -mt-[5.25rem]"
   >
     <div className="max-w-[77.5rem] mx-auto px-5 md:px-10 lg:px-15 xl:max-w-[87.5rem] relative" >
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,27 +1,28 @@
-
-
 import { services } from "../constants";
-
+import homepage from '../assets/homepage.jpg'
 const Service = () => {
   return (
-    <section id="service" className="mt-20 bg-smokewhite">
+    <section id="service" className="mt-20 ">
        
       <div className="max-w-[77.5rem] mx-auto px-5 md:px-10 lg:px-15 xl:max-w-[87.5rem] relative z-2 ">
         <h1 className=" font-semibold text-[2.5rem] leading-[3.25rem] md:text-[2.75rem] md:leading-[3.75rem] lg:text-[3.25rem] lg:leading-[4.0625rem] xl:text-[3.75rem] xl:leading-[4.5rem]">
-          Chat Smarter, Not Harder with Brainwave"
+        Разгледайте Нашите Професионални Услуги за Почистване
         </h1>
-
+        <p className="mt-4 text-md md:text-xl text-center text-gray-500">
+          Влезте в света на чистотата и комфорта с нашата компания. Ние предлагаме разнообразни услуги за почистване,
+          които ще направят вашия дом, офис или друго пространство безупречно чисто и приятно за живеене или работа.
+        </p>
         <div className="flex flex-wrap gap-10 mt-10 mb-10">
           {services.map((item) => (
             <div
               key={item.id}
-              className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]"
+              className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem] hover:scale-105"
               style={{ backgroundImage: `url(${item.imageUrl})` }}
             >
               
               <div className="relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none">
-                <h5 className="h5 mb-5">{item.type}</h5>
-                <p className="body-2 mb-6 text-gray-400"> {item.text}</p>
+                <h5 className="text-2xl leading-normal underline mb-5">{item.type}</h5>
+                <p className="body-2 mb-6 text-gray-600"> {item.text}</p>
                 <div className="flex items-center mt-auto">
                   <img
                     src={item.imageUrl}
@@ -44,7 +45,7 @@ const Service = () => {
                 <div className="absolute inset-0 opacity-0 transition-opacity hover:opacity-10">
                   {item.imageUrl && (
                     <img
-                      src={item.imageUrl}
+                      src={homepage}
                       width={380}
                       height={362}
                       alt={item.title}
