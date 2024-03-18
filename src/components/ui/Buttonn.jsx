88b38b3df2999161c092bbe2af9bfb9581df5556
Velizar Dabov <velizardabov@gmail.com>
2024-03-18 14:16:12 +0200

    footer ready

--- src/components/ui/Buttonn.jsx
+++ src/components/ui/Buttonn.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ButtonSvg from "../../assets/svg/ButtonSvg";
 const Buttonn = ({ className, href, onClick, children, px, white }) => {
-  const classes = `button relative inline-flex items-center justify-center h-11 transition-colors hover:scale-105 ${
+  const classes = `button relative inline-flex items-center justify-center h-11 transition-colors  ${
     px || "px-7"
   } ${white ? "text-blue-500" : "text-white"} ${className || ""}`;
   const spanClasses = `relative z-10`;
