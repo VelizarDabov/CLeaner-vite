22fb620f2df258f3638a1bee4d41b33efa23a4ca
Velizar Dabov <velizardabov@gmail.com>
2024-03-21 09:33:45 +0200

    ok

--- src/components/Foote.jsx
+++ src/components/Foote.jsx
@@ -3,7 +3,7 @@ import { socials } from "../constants/index";
 
 const Footer = () => {
   return (
-    <section crosses className="!px-0 !py-10">
+    <section  className="!px-0 !py-10">
       <div className=" flex sm:justify-between justify-center items-center gap-10 max-sm:flex-col">
         <p className=" text-n-4 lg:block">
           © {new Date().getFullYear()}. All rights reserved.
